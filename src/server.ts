58b57ee245d9cf4e98d14294ccd85f85b16ef1c6
Jose David <lujan9495@gmail.com>
2024-05-20 14:05:28 -0500

    Connecting the express application to a database and setting the DB connection in environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,20 @@
 import express from 'express';
 import router from './routes';
+import db from './config/db';
+
+// Connect to the database
+async function connectDb() {
+    try {
+      await db.authenticate();
+      db.sync();
+      console.log("Connected to database");
+    } catch (err) {
+      console.log(err);
+      console.log("Error connecting to database");
+    }
+}
+
+connectDb();
 
 const app = express();
 
