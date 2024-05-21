645e75f7da9479e7e96c1e97544f147477202365
Jose David <lujan9495@gmail.com>
2024-05-20 21:13:19 -0500

    Creating DB model

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import app from './server';
+import colors from 'colors';
 
+// Set the port
 const PORT = process.env.PORT || 3000;
 
+// Start the server
 app.listen(3000, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(colors.cyan.bold(`Server running on port ${PORT}`));
 });
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 import router from './routes';
 import db from './config/db';
+import colors from 'colors';
 
 // Connect to the database
 async function connectDb() {
     try {
       await db.authenticate();
       db.sync();
-      console.log("Connected to database");
+      console.log(colors.cyan('Conectado a la base de datos'));
     } catch (err) {
       console.log(err);
-      console.log("Error connecting to database");
+      console.log(colors.red.bold('No se pudo conectar a la base de datos'));
     }
 }
 
