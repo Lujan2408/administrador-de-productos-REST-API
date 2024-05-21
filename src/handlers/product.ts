5eb1e7f856c7e8f558182314241930eff0944df3
Jose David <lujan9495@gmail.com>
2024-05-21 13:48:52 -0500

    Validating Create Product on the server

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,15 +1,26 @@
 import { Request, Response } from "express";
 import Product from "../models/Product.model";
+import { check, validationResult } from 'express-validator';
+
 
 // Siempre que interactuamos con un modelo, es necesario que la funcion sea asincrona 
-
 export const createProduct = async (req : Request, res : Response) => {
   
-  // Insertar datos en la base de datos (1 forma)
-  // const product = new Product(req.body);
-  // const savedProduct = await product.save();
+  // Validar datos
+  await check('name').trim().notEmpty().withMessage('El nombre del producto no puede estar vacio o contener espacios en blanco').run(req); 
+  
+  await check('price')
+    .isNumeric().withMessage('Valor no válido')
+    .notEmpty().withMessage('El precio del producto no puede estar vacio')
+    .custom(value => value > 0).withMessage('El precio del producto no puede ser negativo')
+    .run(req);
 
-  // Insertar datos en la base de datos (2 forma)
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()});
+  }
+
+  // Insertar datos en la base de datos
   const product = await Product.create(req.body); 
   
   res.json({data: product});
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import router from './routes';
+import router from './router';
 import db from './config/db';
 import colors from 'colors';
 
