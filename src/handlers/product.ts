a345fb87bc936c4392ed9ee6823d6acd57614aa0
Jose David <lujan9495@gmail.com>
2024-05-21 21:17:57 -0500

    Creating Endpoint for getting products from DB

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,12 +1,27 @@
 import { Request, Response } from "express";
 import Product from "../models/Product.model";
-
+import colors from "colors";
 
 // Siempre que interactuamos con un modelo, es necesario que la funcion sea asincrona 
-export const createProduct = async (req : Request, res : Response) => {
 
-  // Insertar datos en la base de datos
-  const product = await Product.create(req.body); 
-  
-  res.json({data: product});
-}
+export const getProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await Product.findAll({ // Consultar todos los productos
+      order: [['price', 'DESC']], 
+      attributes: {exclude: ['createdAt', 'updatedAt', 'availability']} 
+    }); 
+    
+    res.json({data: products}); 
+  } catch (error) {
+    console.log(colors.bgRed(error));
+  }
+};
+
+export const createProduct = async (req: Request, res: Response) => {
+  try {
+    const product = await Product.create(req.body); // Insertar datos en la base de datos
+    res.json({ data: product });
+  } catch (error) {
+    console.log(colors.bgRed(error));
+  }
+};
